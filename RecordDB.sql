0c44509710804ec3dda3751a473d1a0d4997dd1b
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2024-02-12 17:33:39 +0600

    add employee.

---
+++ RecordDB.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emp_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `birthplace` varchar(255) NOT NULL,
+  `status` varchar(15) NOT NULL
+) 
+-- ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `employee`
+--
+-- ALTER TABLE `employee`
+--   ADD PRIMARY KEY (`emp_id`);
+
+-- --
+-- -- AUTO_INCREMENT for table `employee`
+-- --
+-- ALTER TABLE `employee`
+--   MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT;
+-- COMMIT;
---
+++ employeeInfo.py
@@ -0,0 +1,24 @@
+import pymysql
+
+connection = pymysql.connect(
+    host        = 'localhost',      
+    user        = 'root',
+    password    = '',
+    db          = 'recordDB',
+)
+
+""" Inserting Record in Database"""
+def AddEmployee():
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO `employee` (`emp_id`, `name`, `gender`, `address`, `birthdate`, `birthplace`, `status`) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+        print(cursor, sql)
+        try:
+            cursor.execute(sql, ('2', 'Atikur rahman', "male", "dhaka", "08.08.1900", "rangpur", "unmarried"))
+            print("New Record Successfully Added!")
+        except Exception as e:
+            print("Fail to add data.", e)
+
+    connection.commit()
+
+    
+AddEmployee()
